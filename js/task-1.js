83c6942267b1d804a180c5ed9e75086047bda796
SobDim <101928077+SobDim@users.noreply.github.com>
2024-02-20 23:33:30 +0200

    m

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,10 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+//* З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+//* Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+
+const listOfCategories = document.querySelector('#categories');
+const category = listOfCategories.querySelectorAll('.item');
+const nameCategory = category.firstElementChild;
+console.log(category.length);
+console.log(nameCategory);
