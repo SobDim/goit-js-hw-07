b0b962c7562e6c5dd0ae5dc591e3b4e7e9213aeb
SobDim <101928077+SobDim@users.noreply.github.com>
2024-02-22 00:37:21 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -4,17 +4,24 @@
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
 const listOfCategories = document.querySelector('#categories');
-const category = listOfCategories.querySelectorAll('.item');
+const category = document.querySelectorAll('.item');
 
-console.log(
-  ` категорій в списку => ${category.length} \n=========================`
-);
+console.log(` категорій в списку => ${category.length} `);
 
+console.log(category);
 const titleCategory = document.querySelectorAll('.item h2');
 
-console.log(titleCategory);
+category.forEach(liStItem => {
+  const title = liStItem.querySelector('h2').textContent;
+  const subcategory = liStItem.querySelectorAll('li');
+  // console.log(liStItem);
+  console.log(title);
+  console.log(subcategory.length);
+});
 
-category.forEach(element => {});
+// titleCategory.forEach(el => {
+//   console.log(el);
+// });
 
 // console.log(listOfCategories);
 
