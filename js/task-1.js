1a81b6a1b5db03dee956b08f258bcee8acf883c1
SobDim <101928077+SobDim@users.noreply.github.com>
2024-02-22 00:53:16 +0200

    +1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 //* З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
 //* Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+//* Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
 const listOfCategories = document.querySelector('#categories');
 const category = document.querySelectorAll('.item');
@@ -14,18 +14,6 @@ const titleCategory = document.querySelectorAll('.item h2');
 category.forEach(liStItem => {
   const title = liStItem.querySelector('h2').textContent;
   const subcategory = liStItem.querySelectorAll('li');
-  // console.log(liStItem);
   console.log(title);
-  console.log(subcategory.length);
+  console.log(`кількість підпунктів ${subcategory.length}`);
 });
-
-// titleCategory.forEach(el => {
-//   console.log(el);
-// });
-
-// console.log(listOfCategories);
-
-// console.log(category);
-// ;
-// const titleText = titleCategory.textContent.join(' ');
-// console.log(titleText);
